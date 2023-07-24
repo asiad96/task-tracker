84147d74253ba2156e5f72cd523f5649ce95c12a
Anastasia <adwarica96r@email.here>
2023-07-24 15:21:52 -0500

    Feature 7 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -24,6 +24,7 @@ def redirect_to_list_projects(request):
 
 
 urlpatterns = [
+    path("accounts/", include("accounts.urls")),
     path("projects/", include("projects.urls")),
     path("", redirect_to_list_projects, name="home"),
     path("admin/", admin.site.urls),
